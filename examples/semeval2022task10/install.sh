62e53e30345bd34dfde215101aaeacff16c27b02
73bc494f <>
2023-12-08 09:21:45 +0900

    fix packaging issue

--- examples/semeval2022task10/install.sh
+++ examples/semeval2022task10/install.sh
@@ -3,12 +3,9 @@
 
 set -eu
 
-./install.sh
-
 # Install packages
 pip install pip==20.2
-pip install torch==1.9.0 transformers==4.15.0 protobuf==3.16.0
-pip install lxml tqdm stanza==1.1.1 dl-translate==0.2.5 pyconll==3.1.0
+pip install -r examples/semeval2022task10/requirements.txt
 
 # Required for seq2seq parser
 pip install -r examples/semeval2022task10/seq2seq_parser/requirements.txt
