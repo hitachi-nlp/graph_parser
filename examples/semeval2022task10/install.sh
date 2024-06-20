72380608129a81b51a695c2563ca9cead38974a3
73bc494f <>
2024-06-21 07:47:14 +0900

    fix numpy package issue

--- examples/semeval2022task10/install.sh
+++ examples/semeval2022task10/install.sh
@@ -5,6 +5,9 @@ set -eu
 
 # Install packages
 pip install pip==20.2
+pip install Cython==0.29.37
+pip install numpy==1.19.0 --no-build-isolation
+
 pip install -r examples/semeval2022task10/requirements.txt
 
 # Required for seq2seq parser
